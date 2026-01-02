2150b7a4694be0f697fdb89d04dd33814ee26656
mauricioneba <117760044+mneba@users.noreply.github.com>
2026-01-02 12:41:29 -0300

    apagamento

--- src/app/[locale]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getLocale } from 'next-intl/server';
-
-export default async function HomePage() {
-  const locale = await getLocale();
-  redirect(`/${locale}/login`);
-}
--- src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Logo } from './Logo';
-export { LanguageSwitcher } from './LanguageSwitcher';
