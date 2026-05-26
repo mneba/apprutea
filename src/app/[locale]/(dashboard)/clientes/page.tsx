ebdc0053da24f2a042b5086477bab31aa7a68da3
mauricioneba <117760044+mneba@users.noreply.github.com>
2026-05-26 18:43:56 -0300

    x

--- src/app/[locale]/(dashboard)/clientes/page.tsx
+++ src/app/[locale]/(dashboard)/clientes/page.tsx
@@ -503,7 +503,7 @@ export default function ClientesPage() {
         </div>
 
         {/* Filtros e Controles */}
-            <div className="flex flex-col sm:flex-row gap-3"></div>
+              <div className="flex flex-col sm:flex-row gap-3">
           {/* Busca */}
           <div className="relative flex-1">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
